2a78b6c4020a30535ba194da718283b2c6f71d49
Damian Kęska <372403+keskad@users.noreply.github.com>
2025-09-23 18:05:24 +0200

    fix: Wait longer for pipelines to finish in tests, do not release on PRs

--- pkgs/testpkg/helpers.go
+++ pkgs/testpkg/helpers.go
@@ -1,8 +1,10 @@
 package testpkg
 
 import (
-	"github.com/sirupsen/logrus"
 	"os/exec"
+	"time"
+
+	"github.com/sirupsen/logrus"
 )
 
 func Kubectl(argv []string) error {
@@ -19,8 +21,10 @@ func Kubectl(argv []string) error {
 
 func WaitForPipelineFinishedByName(ns string, name string) {
 	Kubectl([]string{"wait", "--for=condition=Succeeded", "pipelinerun", "-n", ns, name})
+	time.Sleep(5 * time.Second)
 }
 
 func WaitForPipelineFinishedByLabel(ns string, label string) {
 	Kubectl([]string{"wait", "--for=condition=Succeeded", "pipelinerun", "-n", ns, "-l", label})
+	time.Sleep(5 * time.Second)
 }
